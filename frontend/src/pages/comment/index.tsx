a50167e731c3c2ef7b733d302ddf4a4171fd8cfe
npv2k1 <npv2k1@gmail.com>
2022-08-16 11:26:47 +0700

    init

---
+++ frontend/src/pages/comment/index.tsx
@@ -0,0 +1,71 @@
+import React, { Children, ReactNode } from "react";
+
+type CommentProps = {
+  comment: string;
+  children?: ReactNode;
+};
+
+const Comment = ({ comment, children }: CommentProps) => {
+  return (
+    <div>
+      <p>Hello {comment}</p>
+      <div className="ml-5">{children}</div>
+    </div>
+  );
+};
+
+const CommentPage = () => {
+  const listComment = [
+    {
+      text: "Hello",
+      comment: [
+        {
+          text: "Hello a",
+          comment: [
+            {
+              text: "Hello b",
+              comment: [],
+            },
+          ],
+        },
+        {
+          text: "Hello d",
+          comment: [
+            {
+              text: "Hello e",
+              comment: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Hello",
+      comment: [
+        {
+          text: "Hello c",
+          comment: [
+            {
+              text: "Hello d",
+              comment: [],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  // Render comment
+  const renderComment = (comment: any) => {
+    return comment.map((item: any) => {
+      return (
+        <div>
+          <Comment comment={item.text} children={renderComment(item.comment)} />
+        </div>
+      );
+    });
+  };
+
+  return <div>{renderComment(listComment)}</div>;
+};
+
+export default CommentPage;
